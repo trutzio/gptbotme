9bf9225e0dc8c178ec8595ebcc1c770fd759fe30
Christian Trutz <info@trutz-software-consulting.de>
2023-02-05 18:30:19 +0100

    chore: Prometheus funktioniert jetzt

--- src/gptbotme.py
+++ src/gptbotme.py
@@ -8,6 +8,7 @@ from tweets_stream import TweetsStream
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
+    start_http_server(8000)
     tweets_stream = TweetsStream(openai_service=OpenAIService())
     tweets_stream.filter()
 
